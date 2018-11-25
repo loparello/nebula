9222929841f1e44e30101131189f5eb1bc7dfc5d
loparello <surferdrugo@gmail.com>
2018-11-25 12:35:48 +0000

    Improved intro jumbotron and header style

--- src/js/components/Jumbotron.vue
+++ src/js/components/Jumbotron.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="intro-jumbotron jumbotron">
-        <h1 class="display-3">Welcome to Nebula!</h1>
-        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr class="my-4">
+    <div class="intro-jumbotron jumbotron text-center">
+        <h1 class="display-4">Welcome to Nebula</h1>
+        <p class="lead">A simple Django Framework strater toolkit for apps, websites and blogs development, empowered with Vue.js on the front-end</p>
+        <hr class="my-4 bg-light">
         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
         <p class="lead">
             <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
