1acd59d820a5aca90084e8c3ec143ab53bda8f8a
loparello <surferdrugo@gmail.com>
2018-11-25 00:26:45 +0000

    Installed bootstrap, jquery and popper.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
+
 import TestComponent from './components/TestComponent';
 
 import '../sass/main.scss';
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (error) {
+    console.error(error);
+}
 
 const app = new Vue({
     el: '#app',
