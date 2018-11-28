b7772783afbc3a44f6fc0f57360e8d29f5e19d0b
loparello <surferdrugo@gmail.com>
2018-11-28 22:28:49 +0000

    Created page and page seo models

--- pages/models.py
+++ pages/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
+class Seo(models.Model):
+    label = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    keywords = models.CharField(max_length=200, blank=True)
+    description = models.TextField(max_length=300, blank=True)
+    def __str__(self):
+        return self.label
+
 class Page(models.Model):
     seo = models.OneToOneField(
-        'Seo', 
+        Seo, 
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
@@ -10,10 +18,9 @@ class Page(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField()
     body = models.TextField(blank=True)
-    image_cover = models.ImageField(upload_to='images/%Y/%m/%d/')
+    image_cover = models.ImageField('Cover', upload_to='images/%Y/%m/%d/')
+    is_published = models.BooleanField('Published', default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
-
-
