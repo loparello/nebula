22be2232aec33af5984605b25c489460d0eb4364
loparello <surferdrugo@gmail.com>
2018-11-28 01:45:16 +0000

    Created page model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Page(models.Model):
+    seo = models.OneToOneField(
+        'Seo', 
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    body = models.TextField(blank=True)
+    image_cover = models.ImageField(upload_to='images/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
