e4147f5ff856b5ecd9d3fc9a5ba4dd974f5e7c60
loparello <surferdrugo@gmail.com>
2018-12-02 18:28:14 +0000

    Added prepopulate page slug from title

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import PageMetadata
-from .models import Page
+from .models import PageMetadata, Page
+
+class PageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(PageMetadata)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
+
+
