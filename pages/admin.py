8c05497dbc6ba39a226d616b16cc4b6c0ffe679f
loparello <surferdrugo@gmail.com>
2018-12-09 18:57:35 +0000

    Set page admin list display and added list filters

--- pages/admin.py
+++ pages/admin.py
@@ -4,6 +4,8 @@ from .models import PageMetadata, Page
 
 class PageAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['title']}
+    list_display = ('title', 'slug', 'is_published', 'is_homepage')
+    list_filter = ('is_published', 'is_homepage')
 
 admin.site.register(PageMetadata)
 admin.site.register(Page, PageAdmin)
